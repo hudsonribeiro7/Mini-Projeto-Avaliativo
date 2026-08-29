01d52c633d1de1fc6fa0f41b6ee1ced4a477d5cc
Hudson Ribeiro <hudsonriberio7@gmail.com>
2026-08-29 20:29:53 -0300

    feat: cria classe de catalogo
    Adiciona catálogo de Pokémon

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { buscarPokemon } from "./services/PokeApiService";
+import { CatalogoPokemon } from "./services/BoxService";
 
 async function main() {
-  const pokemon = await buscarPokemon("pikachu");
+  const catalogo = new CatalogoPokemon();
 
-  console.log(pokemon);
+  const pikachu = await buscarPokemon("pikachu");
+
+  if (pikachu !== null) {
+    catalogo.adicionar(pikachu);
+  }
+
+  catalogo.listar();
+
+catalogo.remover(999);
+
+  catalogo.listar();
 }
 
 main();
